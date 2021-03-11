c2e635a132e430ad875c3c97539169a0bef0c215
wmiguel29 <69988821+wmiguel29@users.noreply.github.com>
2021-03-10 21:23:25 -0500

    Update Administracion.java

--- Administracion.java
+++ Administracion.java
@@ -1,5 +1,21 @@
 public class Administracion{
-  
+  Persona[] personas;
+	
+	
+	public void escribirObjeto(String direccion) throws IOException {
+		for (int i = 0; i < personas.length; i++) {
+			personas[i].escribirObjeto(direccion+"\\" + personas[i].getNombre+".clt");
+		}
+	}
+	
+	public Persona[] leerObjetos(File f) throws ClassNotFoundException, IOException {
+		File[] listF=f.listFiles(new Filtro(".clt"));
+		Persona[] pers= new Persona[listF.length];
+		for (int i = 0; i < pers.length; i++) {
+			pers[i].leerObjeto(listF[i]);
+		}
+		return pers;
+		}
 }
 
   
